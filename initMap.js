20b24240d64eb116db2c0bcf0801973359004077
Robert McWilliam <rmcw@allmail.net>
2020-08-07 23:46:44 +0100

    Get toggleable categories working

--- initMap.js
+++ initMap.js
@@ -1,13 +1,104 @@
 // Initialize and add the map
-function initMap() {
-  var markerImg = document.querySelector('.markerImage').innerHTML;
-    console.log(markerImg);  
-  var center = {lat: 57.06, lng: -2.42};
-  // The map, centered at center
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 12, center: center, mapTypeId: 'satellite'});
-  // The marker, positioned at Uluru
-    for(m of data) {
+var natural_markers = [];
+var natural_colour = '#f8edeb';
+var chunk_markers = [];
+var chunk_colour = '#fdffb6';
+var retouched_markers = [];
+var retouched_colour = '#ffc6ff';
+var blade_markers = [];
+var blade_colour = '#a0c4ff';
+var flake_markers = [];
+var flake_colour = '#ffd6a5';
+var core_markers = [];
+var core_colour = '#ffadad';
+var other_markers = [];
+var other_colour = '#e5e5e5';
+
+var map;
+
+var cat_markers = {'core': core_markers,
+                   'flake': flake_markers,
+                   'blade': blade_markers,
+                   'retouched': retouched_markers,
+                   'chunk': chunk_markers,
+                   'natural': natural_markers,
+                   'other': other_markers}
+
+var core_active = true;
+var flake_active = true;
+var blade_active = true;
+var retouched_active = true;
+var chunk_active = true;
+var natural_active = true;
+var other_active = true;
+
+
+
+var categories = [['core', core_markers, core_colour],
+                  ['flake', flake_markers, flake_colour],
+                  ['blade', blade_markers, blade_colour],
+                  ['retouched', retouched_markers, retouched_colour],
+                  ['chunk', chunk_markers, chunk_colour],
+                  ['natural', natural_markers, natural_colour],
+                  ['other', other_markers, other_colour]]
+function removeMarkers(markers) {
+    for(m of markers) {
+        m.setMap(null);
+    }
+
+}
+
+function restoreMarkers(markers) {
+    for(m of markers) {
+        m.setMap(map);
+    }
+}
+
+function toggle_markers(markers, active) {
+    if(active) {
+        removeMarkers(markers);
+    } else {
+        restoreMarkers(markers);
+    }
+}
+
+
+function toggle_core() {
+    console.log("toggle core")
+    toggle_markers(core_markers, core_active);
+    core_active = !core_active;
+}
+function toggle_flake() {
+    toggle_markers(flake_markers, flake_active);
+    flake_active = !flake_active;
+}
+function toggle_blade() {
+    toggle_markers(blade_markers, blade_active);
+    blade_active = !blade_active;
+}
+function toggle_retouched() {
+    toggle_markers(retouched_markers, retouched_active);
+    retouched_active = !retouched_active;
+}
+function toggle_chunk() {
+    toggle_markers(chunk_markers, chunk_active);
+    chunk_active = !chunk_active;
+}
+function toggle_natural() {
+    toggle_markers(natural_markers, natural_active);
+    natural_active = !natural_active;
+}
+function toggle_other() {
+    toggle_markers(other_markers, other_active);
+    other_active = !other_active;
+}
+
+
+function addMarkers(points, markers, colour) {
+    var markerImg = document.querySelector('.markerImage').innerHTML;
+    // console.log(markerImg);  
+
+     for(m of points) {
         var contentString = '<div id="content">'+
             '<h2>'+m['type']+'</h2>'+
             '<table style="padding: 5px;"><tr>'+
@@ -29,12 +120,24 @@ function initMap() {
             map: map,
             icon: {
                 anchor: new google.maps.Point(24, 24),
-                url: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(markerImg.replace('{{colour}}', m['colour'])),
+                url: 'data:image/svg+xml;charset=utf-8,' + encodeURIComponent(markerImg.replace('{{colour}}', colour)),
                 scaledSize: new google.maps.Size(24, 24)
             }
         });
-        addInfo(marker, contentString);
-       
+         addInfo(marker, contentString);
+         markers.push(marker);
+     }
+}
+
+
+function initMap() {
+    var center = {lat: 57.06, lng: -2.42};
+    // The map, centered at center
+    map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 12, center: center, mapTypeId: 'satellite'});
+    
+    for(c of categories) {
+        addMarkers(data[c[0]], c[1], c[2]);
     }
 }
 
@@ -44,5 +147,6 @@ function addInfo(marker, contentString) {
         infoWindow.open(map, marker);
     });
 }
-    
+
+
 
